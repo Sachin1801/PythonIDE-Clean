6a988f103cf7b073164527904d627f7b5a5c5e41
Sachin Adlakha <sachinadlakha7@gmail.com>
2025-09-15 14:31:09 -0400

    Add GitHub Actions + reassign test accounts

--- server/projects/ide/Local/aas10176/welcome.py
+++ server/projects/ide/Local/aas10176/welcome.py
@@ -1,8 +1,8 @@
-# Welcome test_1!
+# Welcome aas10176!
 # This is your personal workspace directory.
 # Only you and the teaching staff can access files here.
 
-print("Hello test_1!")
+print("Hello aas10176!")
 print("This is your personal Python IDE workspace.")
 print("Start coding and have fun learning Python!")
 
--- server/projects/ide/Local/bh2854/welcome.py
+++ server/projects/ide/Local/bh2854/welcome.py
@@ -1,8 +1,8 @@
-# Welcome test_2!
+# Welcome bh2854!
 # This is your personal workspace directory.
 # Only you and the teaching staff can access files here.
 
-print("Hello test_2!")
+print("Hello bh2854!")
 print("This is your personal Python IDE workspace.")
 print("Start coding and have fun learning Python!")
 

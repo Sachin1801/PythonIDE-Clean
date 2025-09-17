93c33a25a09ee80f414b19b0200870eb87f9dcf4
Sachin Adlakha <sachinadlakha7@gmail.com>
2025-09-17 19:44:00 -0400

    Checkpoint: test_3 and test_4 used and created, Login modal fixed to remove sa9082 and Student ID to Net ID

--- server/projects/ide/Local/emm10201/welcome.py
+++ server/projects/ide/Local/emm10201/welcome.py
@@ -1,8 +1,8 @@
-# Welcome test_3!
+# Welcome emm10201!
 # This is your personal workspace directory.
 # Only you and the teaching staff can access files here.
 
-print("Hello test_3!")
+print("Hello emm10201!")
 print("This is your personal Python IDE workspace.")
 print("Start coding and have fun learning Python!")
 
